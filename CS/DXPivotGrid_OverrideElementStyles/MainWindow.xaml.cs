49dfd133e7dd05c4dfbdb870a76dc33209e02915
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/DXPivotGrid_OverrideElementStyles/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+using DXPivotGrid_OverrideElementStyles.DataSet1TableAdapters;
+
+namespace DXPivotGrid_OverrideElementStyles {
+    public partial class MainWindow : Window {
+        SalesPersonTableAdapter salesPersonDataAdapter = new SalesPersonTableAdapter();
+        public MainWindow() {
+            InitializeComponent();
+            pivotGridControl1.DataSource = salesPersonDataAdapter.GetData();
+        }
+    }
+}
---
+++ VB/DXPivotGrid_OverrideElementStyles/MainWindow.xaml.vb
@@ -0,0 +1,14 @@
+﻿Imports System.Windows
+Imports DXPivotGrid_OverrideElementStyles.DataSet1TableAdapters
+
+Namespace DXPivotGrid_OverrideElementStyles
+    Partial Public Class MainWindow
+        Inherits Window
+
+        Private salesPersonDataAdapter As New SalesPersonTableAdapter()
+        Public Sub New()
+            InitializeComponent()
+            pivotGridControl1.DataSource = salesPersonDataAdapter.GetData()
+        End Sub
+    End Class
+End Namespace
